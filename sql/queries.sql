10647cdf776b1dd501727c8f1568387ef9db13f6
Daniel Eineving <daniel@eineving.se>
2015-05-24 23:30:40 +0200

    Fixing sql querries

--- sql/queries.sql
+++ sql/queries.sql
@@ -11,11 +11,13 @@ ORDER BY Brutto
 
 
 /*HCP*/
-SELECT roundStart,  firstname, lastname, hcp 
-FROM Score, teerating, round, player
+SELECT roundStart,  firstname, lastname, round.hcp, round.teeName, teerating.cr, teerating.slope, 
+	-round.hcp*(teerating.slope/113)+(teerating.cr-sum(hole.par)) AS SHCP, sum(hole.par) AS Par
+FROM Score, teerating, round, player, Hole
 WHERE Score.roundStart = round.dateAndTime AND
 	round.teeName = teerating.teeName AND
     player.sex = teerating.sex AND
     player.golfid = round.player AND
-    player.golfid = Score.player
+    player.golfid = Score.player AND
+    Score.number = hole.number
 GROUP BY concat(player.golfID, round.dateAndTime);
